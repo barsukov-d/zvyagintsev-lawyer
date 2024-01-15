395826be5231fff5f3b9d759b63b2211e7a9fcf9
Barsukov D <b.dmitry@alef.im>
2024-01-15 16:15:13 +0400

    Add prettier-plugin-tailwindcss and update default layout

---
+++ components/PageHeader.vue
@@ -0,0 +1,16 @@
+<template>
+	<header class="h-20">
+		<nav class="container mx-auto flex h-full items-center justify-center px-4 sm:px-6 lg:px-8">
+			<ul class="flex items-center justify-center gap-8">
+				<li>
+					<nuxt-link to="/" class="">Home</nuxt-link>
+				</li>
+				<li>
+					<nuxt-link to="/about">About</nuxt-link>
+				</li>
+			</ul>
+		</nav>
+	</header>
+</template>
+
+<script setup lang="ts"></script>
